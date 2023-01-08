e1ab34d73cc80d18ced0570fc756dad3b53b97ea
Łukasz Wójcik <lwojcik@lukem.net>
2023-01-08 23:49:48 +0100

    Zapis w localStorage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,38 +1,43 @@
 const switchToDarkLabel = "Włącz tryb ciemny";
 const switchToLightLabel = "Włącz tryb jasny";
+const htmlThemeAttribute = "data-theme";
+const themeKey = "theme";
+const darkValue = "dark";
+const lightValue = "light";
 
-const switchToDark = (htmlElement) => {
-  document.querySelector("html").setAttribute("data-theme", "dark");
+const switchToDark = () => {
+  document.querySelector("html").setAttribute(htmlThemeAttribute, darkValue);
   document.querySelector(".theme-switcher").innerText = switchToLightLabel;
 };
 
 const switchToLight = () => {
-  document.querySelector("html").setAttribute("data-theme", "light");
+  document.querySelector("html").setAttribute(htmlThemeAttribute, lightValue);
   document.querySelector(".theme-switcher").innerText = switchToDarkLabel;
 };
 
+const saveThemePreference = (value) => {
+  if (localStorage.setItem) localStorage.setItem(themeKey, value);
+};
+
 window.addEventListener("DOMContentLoaded", () => {
-  const themeKey = "theme";
   const htmlElement = document.querySelector("html");
   const currentTheme = localStorage.getItem(themeKey) || null;
 
-  if (currentTheme === "dark") {
+  if (currentTheme === darkValue) {
     switchToDark();
-  } else if (currentTheme === "light") {
+  } else if (currentTheme === lightValue) {
     switchToLight();
   }
 
-  document
-    .querySelector(".theme-switcher")
-    .addEventListener("click", function () {
-      const currentMode = htmlElement.getAttribute("data-theme");
+  document.querySelector(".theme-switcher").addEventListener("click", () => {
+    const currentMode = htmlElement.getAttribute("data-theme");
 
-      if (currentMode === "light") {
-        switchToDark(htmlElement);
-        if (localStorage.setItem) localStorage.setItem(themeKey, "dark");
-      } else {
-        switchToLight(htmlElement);
-        if (localStorage.setItem) localStorage.setItem(themeKey, "light");
-      }
-    });
+    if (currentMode === lightValue) {
+      switchToDark(htmlElement);
+      saveThemePreference(darkValue);
+    } else if (currentMode === darkValue) {
+      switchToLight(htmlElement);
+      saveThemePreference(lightValue);
+    }
+  });
 });
